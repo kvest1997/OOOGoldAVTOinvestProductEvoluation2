9ad6f92f6a7f055e7c37499664a72cbd8583989e
Kirill <kvest1997@gmail.com>
2022-04-06 17:53:28 +0700

    Создание библиотеки Support

--- Support/GetRequest.cs
+++ Support/GetRequest.cs
@@ -13,11 +13,18 @@ namespace OOOGoldAVTOinvestProductEvoluation2
         HttpWebRequest _request;
         string _address;
 
+        public Dictionary<string, string> Headers { get; set; }
+
         public string Response { get; set; }
+        public string Accept { get; set; }
+        public string Host { get; set; }
+        public WebProxy Proxy { get; set; }
+
 
         public GetRequest(string address)
         {
             this._address = address;
+            Headers = new Dictionary<string, string>();
         }
 
         public void Run()
@@ -36,6 +43,32 @@ namespace OOOGoldAVTOinvestProductEvoluation2
             catch (Exception)
             {
 
+            }
+        }
+
+        public void Run(CookieContainer cookieContainer)
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "Get";
+            _request.CookieContainer = cookieContainer;
+            _request.Proxy = Proxy;
+            _request.Accept = Accept;
+            _request.Host = Host;
+
+            foreach (var pair in Headers)
+            {
+                _request.Headers.Add(pair.Key, pair.Value);
+            }
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception)
+            {
             }
         }
     } 
