251c6321cf299cd2bab49fd742a2df2de89a8c59
Kirill <kvest1997@gmail.com>
2022-04-09 13:47:57 +0700

    +Создание классов SiteParser, SiteSettings +реализация интерфейсов IParse IParserSettings

---
+++ OOOGoldAVTOinvestProductEvoluation2/Core/Site/SiteParser.cs
@@ -0,0 +1,27 @@
+﻿using AngleSharp.Html.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOOGoldAVTOinvestProductEvoluation2.Core.Site
+{
+    class SiteParser : IParse<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            var list = new List<string>();
+            var items = document.QuerySelectorAll("h3").Where(item => item.ClassName != null & item.ClassName.Contains("title-root-zZCwT iva-item-title-py3i_ title-listRedesign-_rejR title-root_maxHeight-X6PsH text-text-LurtD text-size-s-BxGpL text-bold-SinUO"));
+        
+            
+            foreach(var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+
+        }
+    }
+}
---
+++ OOOGoldAVTOinvestProductEvoluation2/Core/Site/SiteSettings.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace OOOGoldAVTOinvestProductEvoluation2.Core.Site
+{
+    class SiteSettings : IParserSettings
+    {
+        static string nameType = "22";
+        public string BaseUrl { get; set; } = "https://www.avito.ru/"
+        public string Prefix { get; set; } = $"rossiya?q={nameType}";
+        public int StartPoint { get; set; }
+        public int EndPoint { get; set; }
+        }
+}
